199417e3ce20cdc370c97a4bd3d00875d99f9644
benjamin <619654015@qq.com>
2020-04-02 18:53:51 +0800

    [IMP]字符串全排列

--- src/pers/benjamin/string/FullCharacterString.java
+++ src/pers/benjamin/string/FullCharacterString.java
@@ -14,6 +14,25 @@ public class FullCharacterString {
         System.out.println(result.toString());
     }
 
+    /**
+     * <li>ABC ，固定A i=0,j=0 swap
+     * <li> BC，固定B i=1,j=1 swap
+     * <li>  C，添加ABC i=2,j=2 swap
+     * <li> CB，固定C i=1,j=2 swap
+     * <li>  B，添加ACB i=1,j=2 swap
+     * <li>
+     * <li>BAC，固定B i=0,j=1 swap
+     * <li> AC，固定A i=1,j=1 swap
+     * <li>  C，添加BAC i=2,j=2 swap
+     * <li> CA，固定C i=1,j=2 swap
+     * <li>  A，添加BCA i=1,j=2 swap
+     * <li>
+     * <li>CBA，固定C i=0,j=2 swap
+     * <li> BA，固定B i=1,j=1 swap
+     * <li>  A，添加CBA i=2,j=2 swap
+     * <li> AB，固定A i=1,j=2 swap
+     * <li>  B，添加CAB i=1,j=2 swap
+    */
     public static void permutation(char[] arr, int from, TreeSet<String> result) {
         if (from > arr.length - 1) {
             return;
