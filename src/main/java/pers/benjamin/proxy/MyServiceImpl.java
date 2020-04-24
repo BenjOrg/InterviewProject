219c2f410b78827589640b66c71e01beac921e66
benjamin <619654015@qq.com>
2020-04-24 13:15:38 +0800

    [IMP]转为Maven项目

--- src/main/java/pers/benjamin/proxy/MyServiceImpl.java
+++ src/main/java/pers/benjamin/proxy/MyServiceImpl.java
@@ -2,6 +2,7 @@ package pers.benjamin.proxy;
 
 public class MyServiceImpl implements MyService {
 
+    @Override
     public void fun() {
         System.out.println("impl");
     }
--- src/main/java/pers/benjamin/proxy/cglibproxy/MyInterceptor.java
+++ src/main/java/pers/benjamin/proxy/cglibproxy/MyInterceptor.java
@@ -6,6 +6,8 @@ import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
 public class MyInterceptor implements MethodInterceptor {
+
+    @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
         System.out.println("Proxy");
         return methodProxy.invokeSuper(o, objects);
