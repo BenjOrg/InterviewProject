f5ade208b48ee6566b01068d71498ab1a5507f2e
benjamin <619654015@qq.com>
2020-09-16 09:43:43 +0800

    feat(spi): add comments

--- src/main/java/pers/benjamin/spi/SpiMain.java
+++ src/main/java/pers/benjamin/spi/SpiMain.java
@@ -4,10 +4,22 @@ import java.util.Optional;
 import java.util.ServiceLoader;
 import java.util.stream.StreamSupport;
 
+/**
+ * 使用SPI代替Class.forName加载驱动
+ *
+ * SPI缺点：在查找具体实现到时候，会遍历所有实现类并实例化
+ * 
+ * @author benjamin
+ * @date 2020/9/16 9:27 AM
+ */
 public class SpiMain {
     public static void main(String[] args) {
 
+        // ServiceLoader JDK查找实现类的工具类
         ServiceLoader<SpiCommon> commons = ServiceLoader.load(SpiCommon.class);
+
+        // 根据META-INF中的文件，加载实现类
+        StreamSupport.stream(commons.spliterator(), false).forEach(t -> System.out.println(t.getClass()));
 
         Optional<SpiCommon> spi = StreamSupport.stream(commons.spliterator(), false).findFirst();
 
